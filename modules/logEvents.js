b19bfc912e460ecd04cf1880aea62d00d453f464
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-02-14 13:07:08 -0330

    When enabled, events are logged to disk

--- modules/logEvents.js
+++ modules/logEvents.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const fsPromises = require("fs").promises;
+const path = require("path");
 
 const EventEmitter = require("events");
 class MyEmitter extends EventEmitter {}
@@ -9,6 +10,34 @@ const { DateTime } = require("luxon");
 const { v4: uuidv4 } = require("uuid");
 
 const myEmitter = new MyEmitter();
+
+// Function to write a line to a log file based on current date
+const writeToDisk = async (line) => {
+  const now = DateTime.now();
+
+  // This file is within a subdirectory, but we want to create our file structure from the root - fortunately path.join allows ".." in the path
+  const logFolder = path.join(__dirname, "..", "logs", now.year.toString());
+
+  try {
+    // Check if logs directory exists
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      // If it doesn't, first create the logs directory
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
+    }
+
+    // Check if yearly logs directory exists
+    if (!fs.existsSync(logFolder)) {
+      // If it doesn't, first create the directory
+      await fsPromises.mkdir(logFolder);
+    }
+
+    let fileName = now.toFormat("yyyyMMdd") + "_server_events.log";
+    // Append to the file. The line passed in won't have a newline, so add one here
+    await fsPromises.appendFile(path.join(logFolder, fileName), line + "\n");
+  } catch (e) {
+    console.log("Failed to write to log file: " + e);
+  }
+};
 
 myEmitter.on("ok", async (msg) => {
   // Create a string consisting of a uuid, timestamp, the event type, and the event message
@@ -20,6 +49,7 @@ myEmitter.on("ok", async (msg) => {
     msg;
 
   if (EVENT_CONSOLE) console.log(text);
+  if (EVENT_LOG) writeToDisk(text);
 });
 
 myEmitter.on("error", async (msg) => {
@@ -32,6 +62,7 @@ myEmitter.on("error", async (msg) => {
     msg;
 
   if (EVENT_CONSOLE) console.log(text);
+  if (EVENT_LOG) writeToDisk(text);
 });
 
 // Export the Emitter so other modules can use it to emit() events
